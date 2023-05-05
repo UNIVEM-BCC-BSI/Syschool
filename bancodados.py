c89a8cf602126d76eca0ad163ad206ec3b7893f0
OliviaLZimmermann <132113303+OliviaLZimmermann@users.noreply.github.com>
2023-05-05 14:49:01 -0300

    Add files via upload
    Banco de dados (protótipo 1)

---
+++ bancodados.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+#conexao
+try:
+    conexao = sqlite3.connect('escola.db')
+    print('Banco de dados conectado com sucesso.')
+except sqlite3.Error as i:
+    print(f'Erro ao conectar ao banco de dados: {i}')
+
+#tabela alunos
+try:
+    with conexao:
+        cursor = conexao.cursor()
+        cursor.execute(""" 
+        CREATE TABLE IF NOT EXISTS Alunos (
+            id_aluno INTEGER PRIMARY KEY AUTOINCREMENT,
+            Nome TEXT,
+            Educação Especial TEXT,
+            Turma TEXT,
+            Nota do bimestre REAL,
+            Professor TEXT
+        )""")
+        print('Tabela Alunos criada com sucesso.')
+except sqlite3.Error as i:
+    print(f'Erro ao criar a tabela de alunos: {i}')
